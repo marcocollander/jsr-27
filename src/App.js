f47156fef0d8a7dc63a29206932c88c8909745ab
mkubera <rainteller@tutanota.de>
2022-02-10 18:50:40 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
+const App = () => {
+  // MODEL
+  const INIT_COURSES = [
+    {
+      id: uuidv4(),
+      name: "Frontend",
+    },
+  ];
+  const [courses, setCourses] = useState(INIT_COURSES);
+
+  // UPDATE
+  const addCourse = (e) => {
+    e.preventDefault();
+
+    const { name } = e.currentTarget.elements;
+    const newCourse = { id: uuidv4(), name: name.value };
+
+    setCourses((state) => [...state, newCourse]);
+  };
+
   return (
+    // VIEW
+    // JSX
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={addCourse}>
+          <input type="text" name="name" defaultValue={"Backend"} />
+          <button>Add</button>
+        </form>
+        <ul>
+          {courses.map(({ id, name }) => (
+            <li key={id}>name: {name}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
-}
+};
 
 export default App;
